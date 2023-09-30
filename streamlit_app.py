9ecc3ddc8594c964585853ced60fbfffd1021d4a
morispolanco <morispolanco@gmail.com>
2023-09-30 11:28:36 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,42 +5,51 @@ import openai
 # Título de la aplicación
 st.title("Corrector de Textos en Español con ChatGPT")
 
+# Cargar la clave de la API de OpenAI ingresada por el usuario
+api_key = st.text_input("Ingresar clave de API de OpenAI", type="password")
+
 # Cargar el documento .docx ingresado por el usuario
 uploaded_file = st.file_uploader("Cargar documento (.docx)", type="docx")
 
 # Botón para corregir el texto
 if st.button("Corregir"):
-    if uploaded_file is not None:
-        # Leer el contenido del documento .docx
-        document_text = docx2txt.process(uploaded_file)
-        
-        # Dividir el texto en chunks más pequeños
-        chunk_size = 500  # Tamaño del chunk en caracteres
-        chunks = [document_text[i:i+chunk_size] for i in range(0, len(document_text), chunk_size)]
-        
-        # Corregir cada chunk utilizando ChatGPT
-        corrected_chunks = []
-        for chunk in chunks:
-            # Llamar a la API de OpenAI para corregir el chunk
-            response = openai.Completion.create(
-                engine="text-davinci-003",
-                prompt=chunk,
-                max_tokens=100,
-                
-                n=1,
-                stop=None,
-                temperature=0.7
-            )
+    if api_key:
+        if uploaded_file is not None:
+            # Leer el contenido del documento .docx
+            document_text = docx2txt.process(uploaded_file)
             
-            # Obtener la respuesta generada por ChatGPT
-            corrected_chunk = response.choices[0].text.strip()
-            corrected_chunks.append(corrected_chunk)
-        
-        # Unir los chunks corregidos en un solo texto
-        corrected_text = " ".join(corrected_chunks)
-        
-        # Mostrar el texto corregido
-        st.subheader("Texto corregido:")
-        st.write(corrected_text)
+            # Dividir el texto en chunks más pequeños
+            chunk_size = 500  # Tamaño del chunk en caracteres
+            chunks = [document_text[i:i+chunk_size] for i in range(0, len(document_text), chunk_size)]
+            
+            # Configurar la clave de la API de OpenAI
+            openai.api_key = api_key
+            
+            # Corregir cada chunk utilizando ChatGPT
+            corrected_chunks = []
+            for chunk in chunks:
+                # Llamar a la API de OpenAI para corregir el chunk
+                response = openai.Completion.create(
+                    engine="text-davinci-003",
+                    prompt=chunk,
+                    max_tokens=100,
+                    temperature=0.7,
+                    n=1,
+                    stop=None,
+                    temperature=0.7
+                )
+                
+                # Obtener la respuesta generada por ChatGPT
+                corrected_chunk = response.choices[0].text.strip()
+                corrected_chunks.append(corrected_chunk)
+            
+            # Unir los chunks corregidos en un solo texto
+            corrected_text = " ".join(corrected_chunks)
+            
+            # Mostrar el texto corregido
+            st.subheader("Texto corregido:")
+            st.write(corrected_text)
+        else:
+            st.warning("Por favor, carga un documento antes de corregir.")
     else:
-        st.warning("Por favor, carga un documento antes de corregir.")
+        st.warning("Por favor, ingresa tu clave de API de OpenAI antes de corregir.")
