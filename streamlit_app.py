09d57d78f56f189a7a932ce0d4b9da79b3afdac9
morispolanco <morispolanco@gmail.com>
2023-10-08 00:19:18 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,57 +1,48 @@
 import streamlit as st
 import requests
-import sounddevice as sd
-import soundfile as sf
-import io
+from PIL import Image
 
 # Configuración de las credenciales
-subscription_id = "bd43ff7a-c218-4228-8379-78333b20e73e"
-resource_group = "Texto"
-account_name = "Voa-a-texto-mp"
-endpoint = "https://eastus.api.cognitive.microsoft.com/sts/v1.0/issuetoken"
+subscription_key = "bd43ff7a-c218-4228-8379-78333b20e73e"
+endpoint = "https://cara-recon.cognitiveservices.azure.com/face/v1.0/detect"
 
-# Función para realizar la solicitud de transcripción
-def transcribir_audio(audio_file):
+# Función para realizar la solicitud de reconocimiento facial
+def reconocer_cara(image_file):
     headers = {
-        "Ocp-Apim-Subscription-Key": subscription_id,
-        "Content-Type": "audio/wav"
+        "Ocp-Apim-Subscription-Key": subscription_key,
+        "Content-Type": "application/octet-stream"
     }
-    response = requests.post(endpoint, headers=headers, data=audio_file)
+    response = requests.post(endpoint, headers=headers, data=image_file)
     if response.status_code == 200:
-        return response.text
+        return response.json()
     else:
         return None
 
 # Configuración de la aplicación Streamlit
-st.title("Transcripción de Audio en Tiempo Real")
+st.title("Reconocimiento Facial")
 
-# Inicializar variables
-recording = False
-audio_frames = []
+# Cargar imagen
+image_file = st.file_uploader("Cargar imagen", type=["jpg", "jpeg", "png"])
 
-# Botón para iniciar/detener la grabación
-if st.button("Iniciar/Detener Grabación"):
-    if not recording:
-        recording = True
-        st.write("Grabando...")
-        audio_frames = []
-        # Configurar la grabación de audio desde el micrófono
-        samplerate = 44100
-        duration = 10  # Duración de la grabación en segundos
-        audio = sd.rec(int(samplerate * duration), samplerate=samplerate, channels=1)
-        sd.wait()
-        # Guardar la grabación en un archivo WAV
-        wav_audio = io.BytesIO()
-        sf.write(wav_audio, audio, samplerate)
-        wav_audio.seek(0)
-        # Realizar la transcripción del audio
-        texto_transcrito = transcribir_audio(wav_audio)
-        if texto_transcrito is not None:
-            # Mostrar el texto transcrito
-            st.text("Texto transcrito:")
-            st.write(texto_transcrito)
-        else:
-            st.error("Error al realizar la transcripción del audio.")
+if image_file is not None:
+    # Mostrar la imagen
+    image = Image.open(image_file)
+    st.image(image, caption="Imagen cargada", use_column_width=True)
+
+    # Realizar el reconocimiento facial
+    resultado = reconocer_cara(image_file)
+
+    if resultado is not None:
+        # Mostrar los resultados del reconocimiento facial
+        st.subheader("Resultados del reconocimiento facial:")
+        for cara in resultado:
+            st.write("Cara detectada:")
+            st.write(" - Edad: ", cara["faceAttributes"]["age"])
+            st.write(" - Género: ", cara["faceAttributes"]["gender"])
+            st.write(" - Emoción: ", cara["faceAttributes"]["emotion"])
+            st.write(" - Sonrisa: ", cara["faceAttributes"]["smile"])
+            st.write(" - Gafas: ", cara["faceAttributes"]["glasses"])
+            st.write(" - Barba: ", cara["faceAttributes"]["facialHair"]["beard"])
+            st.write(" - Bigote: ", cara["faceAttributes"]["facialHair"]["moustache"])
     else:
-        recording = False
-        st.write("Grabación detenida.")
+        st.error("Error al realizar el reconocimiento facial.")
