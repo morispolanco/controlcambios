00a8d4e0dfecb642328f8cef2964154dbfa9f1f9
morispolanco <morispolanco@gmail.com>
2023-10-07 21:13:53 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,77 +2,46 @@ import streamlit as st
 from docx import Document
 from googletrans import Translator
 
-def translate_to_english(document):
+def translate_text(text, src_lang, dest_lang):
     translator = Translator()
-    document_text = ""
-    for paragraph in document.paragraphs:
-        document_text += paragraph.text + "\n"
-    translated_text = translator.translate(document_text, src='es', dest='en').text
-    
-    document_en = Document()
-    document_en.add_paragraph(translated_text)
-    
-    return document_en
+    translation = translator.translate(text, src=src_lang, dest=dest_lang)
+    return translation.text
 
-def translate_to_spanish(document_en):
-    translator = Translator()
-    document_text_en = ""
-    for paragraph in document_en.paragraphs:
-        document_text_en += paragraph.text + "\n"
-    translated_text_es = translator.translate(document_text_en, src='en', dest='es').text
+def generate_comparison_doc(original_text, translated_text):
+    doc = Document()
+    doc.add_heading('Comparación entre el documento original y traducido', 0)
     
-    document_es = Document()
-    document_es.add_paragraph(translated_text_es)
+    original_paragraphs = original_text.split('\n\n')
+    translated_paragraphs = translated_text.split('\n\n')
     
-    return document_es
+    for i in range(min(len(original_paragraphs), len(translated_paragraphs))):
+        doc.add_heading('Párrafo {}'.format(i+1), level=1)
+        doc.add_heading('Original:', level=2)
+        doc.add_paragraph(original_paragraphs[i])
+        doc.add_heading('Traducido:', level=2)
+        doc.add_paragraph(translated_paragraphs[i])
+    
+    return doc
 
-def generate_comparison_document(document, document_es):
-    comparison_doc = Document()
-    comparison_doc.add_heading('Comparison between the original and translated documents', level=1)
-    
-    num_paragraphs = min(len(document.paragraphs), len(document_es.paragraphs))
-    
-    for i in range(num_paragraphs):
-        p1 = document.paragraphs[i].text
-        p2 = document_es.paragraphs[i].text
-        
-        if p1 != p2:
-            run = comparison_doc.add_paragraph().add_run(p1)
-            run.bold = True
-            run.underline = True
-            comparison_doc.add_paragraph(p2)
-        else:
-            comparison_doc.add_paragraph(p1)
-    
-    return comparison_doc
+st.title("Traductor de documentos con Streamlit")
 
-st.title("Document Translation App")
-
-uploaded_file = st.file_uploader("Upload a .docx file", type="docx")
+uploaded_file = st.file_uploader("Cargar documento DOCX", type="docx")
 
 if uploaded_file is not None:
     document = Document(uploaded_file)
+    original_text = '\n\n'.join([paragraph.text for paragraph in document.paragraphs])
     
-    st.header("Original Document (Spanish)")
-    for paragraph in document.paragraphs:
-        st.write(paragraph.text)
+    st.header("Texto original (español)")
+    st.write(original_text)
     
-    st.header("Translated Document (English)")
-    document_en = translate_to_english(document)
-    for paragraph in document_en.paragraphs:
-        st.write(paragraph.text)
+    st.header("Texto traducido al inglés")
+    translated_text = translate_text(original_text, 'es', 'en')
+    st.write(translated_text)
     
-    st.header("Translated Document (Back to Spanish)")
-    document_es = translate_to_spanish(document_en)
-    for paragraph in document_es.paragraphs:
-        st.write(paragraph.text)
+    st.header("Texto traducido de nuevo al español")
+    translated_back_text = translate_text(translated_text, 'en', 'es')
+    st.write(translated_back_text)
     
-    st.header("Comparison Document with Track Changes")
-    comparison_doc = generate_comparison_document(document, document_es)
-    comparison_doc.save("comparison_document.docx")
-    st.download_button(
-    label="Download Comparison Document",
-    data=comparison_doc.save("comparison_document.docx").getvalue(),
-    file_name="comparison_document.docx",
-    mime="application/vnd.openxmlformats-officedocument.wordprocessingml.document"
-)
+    st.header("Comparación entre el documento original y traducido")
+    comparison_doc = generate_comparison_doc(original_text, translated_back_text)
+    st.download_button("Descargar documento de comparación", data=comparison_doc.save, file_name="comparison_doc.docx")
